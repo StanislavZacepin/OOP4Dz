6bc722b431f5da2e8c066eb1028dd9bee32aea96
†¤¤† †¤¤† <yaolsher@yandex.ru>
2022-09-09 17:13:29 +0300

    Тестовая запись

--- src/Model/TextWriter.java
+++ src/Model/TextWriter.java
@@ -1,14 +1,24 @@
 package Model;
 
+import Entity.FileTask;
+import Entity.Priority;
+
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.Date;
 
 public class TextWriter <T> {
-    public void textWriter(T object) {
+    public void textWriter(FileTask fileTask) {
         try (FileWriter writer = new FileWriter("notes3.txt", false)) {
 
 
-            writer.write((String) object);
+             Integer id = fileTask.getId();
+             String fio = fileTask.getFio();
+             Priority priority = fileTask.getPriority();
+             Date date = fileTask.getDate();
+             String task = fileTask.getText();
+
+            writer.write("id="+id +" fio="+fio +" priority="+priority +" date="+date +" task="+task);
             // запись по символам
             writer.append('\n');
             writer.append('E');
